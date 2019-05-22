f45e1faf9645817b41acafda160fba757c907870
Luiz Henrique Domingues Soares <luiz.soares@comuniki.me>
2019-05-22 18:53:02 -0300

    setting request options for requests

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,47 @@
-const a = 8
+import * as https from "https";
+import { IncomingMessage, ClientRequest } from "http";
+import { hostname } from "os";
 
-console.log(a)
+class App {
+  public options: https.RequestOptions;
+  public agent: https.Agent;
+
+  // Agent keepAlive true to decrease requests response time
+  public constructor() {
+    this.options = {};
+    this.agent = new https.Agent({
+      keepAlive: true
+    });
+  }
+
+  public callAPI() {
+    console.log(this.options);
+
+    const req: ClientRequest = https.request(this.options, res => {
+      var body: string;
+
+      res.on("data", data => {
+        body += data;
+        process.stdout.write(data);
+      });
+
+      res.on("end", function() {
+        console.log("Body :" + body);
+      });
+    });
+
+    req.on("error", error => {
+      console.log(error.message);
+    });
+
+    req.end();
+  }
+
+  public setOptions(method: string, hostname: string, path: string) {
+    this.options = { method, hostname, path };
+  }
+}
+
+var app = new App();
+app.setOptions("GET", "google.com", "/tag");
+app.callAPI();
