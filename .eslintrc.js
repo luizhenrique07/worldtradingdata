2dee9de5e9dd30abc035260d14d0dbd1b8dc4911
Luiz Henrique Domingues Soares <luizhenrique1929@gmail.com>
2019-05-26 22:31:06 -0300

    adding request class

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,24 @@
 module.exports = {
-  "parser": '@typescript-eslint/parser',
-  'env': {
-    'es6': true,
-    'node': true
+  parser: "@typescript-eslint/parser",
+  env: {
+    es6: true,
+    node: true
   },
-  "plugins": ['@typescript-eslint'],
-  'extends': ['plugin:@typescript-eslint/recommended','standard'],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  plugins: ["@typescript-eslint"],
+  extends: ["plugin:@typescript-eslint/recommended", "standard"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
   },
-  'rules': {
+  rules: {
+    semi: ["error", "always"],
+    indent: "off",
+    camelcase: "off",
+    "@typescript-eslint/indent": ["error", 2],
+    "space-before-function-paren": ["error", "never"],
   }
-}
+};
--- src/server.ts
+++ src/server.ts
@@ -1,46 +1,12 @@
-import * as https from "https";
-import { IncomingMessage, ClientRequest } from "http";
 
-class App {
-  public options: https.RequestOptions;
-  public agent: https.Agent;
+import { resolve } from 'path';
+import { config } from 'dotenv';
+import { RealTime } from './api/realTime';
 
-  // Agent keepAlive true to decrease requests response time
-  public constructor() {
-    this.options = {};
-    this.agent = new https.Agent({
-      keepAlive: true
-    });
-  }
-
-  public callAPI() {
-    console.log(this.options);
-
-    const req: ClientRequest = https.request(this.options, res => {
-      var body: string;
-
-      res.on("data", data => {
-        body += data;
-        process.stdout.write(data);
-      });
-
-      res.on("end", function() {
-        console.log("Body :" + body);
-      });
-    });
-
-    req.on("error", error => {
-      console.log(error.message);
-    });
-
-    req.end();
-  }
-
-  public setOptions(method: string, hostname: string, path: string) {
-    this.options = { method, hostname, path };
-  }
+config({ path: resolve(__dirname, '../.env') });
+if (process.env.API_TOKEN === undefined || process.env.API_TOKEN === '') {
+  throw new Error('API_TOKEN is required');
 }
 
-var app = new App();
-app.setOptions("GET", "google.com", "/tag");
-app.callAPI();
+var realTime = new RealTime();
+realTime.callAPI();
