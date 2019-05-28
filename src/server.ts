eccb2e4f0d25e11a68ea9402f2eeef208e0bf869
Luiz Henrique Domingues Soares <luizhenrique1929@gmail.com>
2019-05-27 22:12:39 -0300

    adding api methods

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 
 import { resolve } from 'path';
 import { config } from 'dotenv';
-import { RealTime } from './api/realTime';
+import { WorldTradingDataWrapper as Wtd } from './request/worldTradingDataWrapper';
 
 config({ path: resolve(__dirname, '../.env') });
 if (process.env.API_TOKEN === undefined || process.env.API_TOKEN === '') {
   throw new Error('API_TOKEN is required');
 }
 
-var realTime = new RealTime();
-realTime.callAPI();
+var wtd = new Wtd();
+var stocks = ['B3SA3.SA'];
+
+async function main(): Promise<void> {
+  var teste: any = await wtd.realTime(stocks);
+  console.log(teste);
+}
+
+main();
