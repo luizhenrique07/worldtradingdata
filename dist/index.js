04bb7f00ab6143426f824ba26454a39f905c03b5
Luiz Henrique Domingues Soares <luizhenrique1929@gmail.com>
2019-06-13 22:35:46 -0300

    fix: remove dist folder

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const worldTradingDataWrapper_1 = require("./request/worldTradingDataWrapper");
-exports.Wtd = worldTradingDataWrapper_1.WorldTradingDataWrapper;
-//# sourceMappingURL=index.js.map
