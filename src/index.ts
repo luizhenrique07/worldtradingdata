824ce1ec1b5ea9ae5d5545c2a343ce6ee909e223
Luiz Henrique Domingues Soares <luizhenrique1929@gmail.com>
2019-07-21 11:35:37 -0300

    feat: export interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import { WorldTradingDataWrapper } from "./worldTradingDataWrapper";
+import { RealTimeResponse } from "./models/realTimeResponse";
+import { IntradayMarketResponse } from "./models/intradayMarketResponse";
+import { HistoricalMarketResponse } from "./models/historicalMarketResponse";
+import { MultiSingleDayHistoryResponse } from "./models/multiSingleDayHistoryResponse";
+import { StockSearchResponse } from "./models/stockSearchResponse";
+import { ForexResponse } from "./models/forexResponse";
+import { ForexHistoryResponse } from "./models/forexHistoryResponse";
+import { ForexSingleDay } from "./models/forexSingleDay";
 
-export { WorldTradingDataWrapper };
+export {
+  WorldTradingDataWrapper,
+  RealTimeResponse,
+  IntradayMarketResponse,
+  HistoricalMarketResponse,
+  MultiSingleDayHistoryResponse,
+  StockSearchResponse,
+  ForexResponse,
+  ForexHistoryResponse,
+  ForexSingleDay
+};
